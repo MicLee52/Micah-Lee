585d934d035d4e89d45dfc48637798c81b6e33bc
Jashaunte Micah Lee <jashaunte.lee@stu.jefferson.kyschools.us>
2018-03-12 08:34:37 -0400

    first commit

---
+++ micah_lee-assign01.py
@@ -0,0 +1,16 @@
+#Micah lee 03/1/18
+
+media_type = input("what is the media type? ")
+title = input("what is the title ")
+des = input("give me a brief description ")
+yr = str(input("what year was it created "))
+rating = float(input(" what rating would you give this media type (1/10) " ))
+new_list = [ title, des, yr, rating ]
+if media_type == "Book":
+    print (new_list)
+elif media_type == "movies":
+     print (new_list)
+
+    
+
+
